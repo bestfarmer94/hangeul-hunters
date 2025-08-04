221cd53915633b484e29bccb596f4d3d86636cf2
shin-kyungyeon <tlsruddus94@gmail.com>
2025-08-04 18:21:53 +0900

    Initial commit

---
+++ src/main/java/com/example/hangeulhunters/HangeulHuntersApplication.java
@@ -0,0 +1,13 @@
+package com.example.hangeulhunters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangeulHuntersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HangeulHuntersApplication.class, args);
+    }
+
+}
