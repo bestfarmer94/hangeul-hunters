cf3510b81bff6423b195e7e615a7aaf53f19608d
shin-kyungyeon <tlsruddus94@gmail.com>
2025-08-17 20:32:38 +0900

    fix : 프롬프트 1차 개편 #2

--- src/main/java/com/example/hangeulhunters/infrastructure/service/naver/dto/EvaluateScoreRequest.java
+++ src/main/java/com/example/hangeulhunters/infrastructure/service/naver/dto/EvaluateScoreRequest.java
@@ -29,7 +29,7 @@ public class EvaluateScoreRequest extends ClovaStructuredRequest<EvaluateScorePr
      */
     public static EvaluateScoreRequest of(String aiRole, String userRole, String situation, String aiMessage, String userMessage) {
         // 시스템 프롬프트 생성
-        String systemPrompt = PromptConstant.HONORIFIC_SLIDER.getPromptMessage() +
+        String systemPrompt =
                 String.format(
                         PromptConstant.EVALUATE_SCORE.getPromptMessage(),
                         aiRole, userRole, situation
